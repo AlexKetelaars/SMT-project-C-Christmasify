87f0d125e8a3dea7cbfe1367f97f06637bec23e1
FIorian <florian.gaeremynck@gmail.com>
2021-02-04 17:16:09 +0100

    something something I can't remember what I did

---
+++ SIAM/GetPatterns.py
@@ -0,0 +1,17 @@
+import sys
+import subprocess
+import os
+import time
+#import psutil
+
+print('sys.argv[1]: ' + sys.argv[1])
+
+for filename in os.listdir(sys.argv[1]):
+    fullname = sys.argv[1] + filename
+    subprocess.call(['java', '-jar', 'OMNISIA20160926.jar', '-i', fullname, '-o', sys.argv[2], '-draw', '-a', 'Forth'])
+    print("hello, yes its me, your python program. I'm still alive and kicking!")
+    time.sleep(2)
+
+# subprocess.call(['java', '-jar', 'OMNISIA20160926.jar', '-i', sys.argv[1], '-o', sys.argv[2], '-draw', '-a', 'Forth', '&'])
+# for proc in psutil.process_iter():
+#     print(proc.name())
