71709d30db2d8484bf398fd590e8e9f952da1aa9
FIorian <florian.gaeremynck@gmail.com>
2021-02-05 14:08:29 +0100

    updating the python script

--- SIAM/GetPatterns.py
+++ SIAM/GetPatterns.py
@@ -8,9 +8,9 @@ print('sys.argv[1]: ' + sys.argv[1])
 
 for filename in os.listdir(sys.argv[1]):
     fullname = sys.argv[1] + filename
-    subprocess.call(['java', '-jar', 'OMNISIA20160926.jar', '-i', fullname, '-o', sys.argv[2], '-draw', '-a', 'Forth'])
+    subprocess.call(['java', '-jar', 'omnisia202101261131.jar', '-i', fullname, '-o', sys.argv[2], '-draw'])
     print("hello, yes its me, your python program. I'm still alive and kicking!")
-    time.sleep(2)
+    #time.sleep(2)
 
 # subprocess.call(['java', '-jar', 'OMNISIA20160926.jar', '-i', sys.argv[1], '-o', sys.argv[2], '-draw', '-a', 'Forth', '&'])
 # for proc in psutil.process_iter():
